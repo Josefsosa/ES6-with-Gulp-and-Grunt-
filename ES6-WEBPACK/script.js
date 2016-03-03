635fbe1b7548c73d2c68e7257453682de8f47c49
tarun <cooltarunsharma111@gmail.com>
2016-03-03 17:19:12 +0530

    initial commit

---
+++ ES6-WEBPACK/script.js
@@ -0,0 +1,11 @@
+var nameBuilder = function(bb="hello", cc="Doe") {
+			console.log(bb + " " + cc);
+		};
+
+nameBuilder();
+
+var nameBuilder1 = function(bb="Jnnnoe", cc="Doe") {
+			console.log(bb + " " + cc);
+		};
+
+nameBuilder1();
